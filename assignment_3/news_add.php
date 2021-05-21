9a7c130b284ccc15e803f88091877106429971db
rijck <r.ijck@hotmail.com>
2021-05-21 17:15:52 +0200

    added template to news_add.php

--- assignment_3/news_add.php
+++ assignment_3/news_add.php
@@ -1,3 +1,17 @@
+<?php
+/* Header */
+$page_title = 'Webprogramming Assignment 3';
+$navigation = Array(
+    'active' => 'News',
+    'items' => Array(
+        'News' => '/WP21/assignment_3/index.php',
+        'Add news item' => '/WP21/assignment_3/news_add.php',
+        'Leap Year' => '/WP21/assignment_3/leapyear.php'
+    )
+);
+include __DIR__ . '/tpl/head.php';
+include __DIR__ . '/tpl/body_start.php';
+?>
 <form action="scripts/add_item.php" method="POST">
     <div class="form-group">
         <label for="title">Title</label>
@@ -9,3 +23,6 @@
     </div>
     <button type="submit" name="submit" class="btn btn-primary">Submit</button>
 </form>
+
+<?php
+include __DIR__ . '/tpl/body_end.php';
